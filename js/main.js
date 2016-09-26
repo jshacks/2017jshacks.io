77ca77666eaa078b7f0c34546b73be4e027a208e
Valentin Tureac <valentin.tureac@elefant.ro>
2016-09-26 22:22:17 +0300

    finish hero section

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ var $menuItem    = $('.js-Menu-item');
 var menuBarHeight = $('.Header-nav').outerHeight();
 var ideaDistance = $('.IdeaSection').position().top - menuBarHeight;
 
-
 window.addEventListener("resize", function() {
   var menuBarHeight = $('.Header-nav').outerHeight();
   var ideaDistance = $('.IdeaSection').position().top - menuBarHeight;
@@ -18,10 +17,12 @@ $(window).resize(function() {
   var ideaDistance = $('.IdeaSection').position().top - menuBarHeight;
 });
 
+
 // mobile menu button
 $menuButton.on('click', function() {
   $body.toggleClass('menu-open');
 });
+
 
 // show menu-bar
 $window.scroll(function() {
@@ -41,98 +42,98 @@ $menuItem.on('click', function() {
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // countdown
-// var deadline = 'October 21 2016 23:59:59 GMT+0300';
+var deadline = 'October 21 2016 23:59:59 GMT+0300';
 
-// function getTimeRemaining(deadline) {
-//   var t = Date.parse(deadline) - Date.parse(new Date());
-//   var seconds = Math.floor((t / 1000) % 60);
-//   var minutes = Math.floor((t / 1000 / 60) % 60);
-//   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-//   var days = Math.floor(t / (1000 * 60 * 60 * 24));
-//   return {
-//     'total': t,
-//     'days': days,
-//     'hours': hours,
-//     'minutes': minutes,
-//     'seconds': seconds
-//   };
-// }
+function getTimeRemaining(deadline) {
+  var t = Date.parse(deadline) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
 
-// function initializeClock(id, deadline) {
-//   var clock = document.getElementById(id);
-//   var daysSpan = clock.querySelector('.js-countdown-days');
-//   var hoursSpan = clock.querySelector('.js-countdown-hours');
-//   var minutesSpan = clock.querySelector('.js-countdown-minutes');
-//   var secondsSpan = clock.querySelector('.js-countdown-seconds');
+function initializeClock(id, deadline) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.js-Countdown-days');
+  var hoursSpan = clock.querySelector('.js-Countdown-hours');
+  var minutesSpan = clock.querySelector('.js-Countdown-minutes');
+  var secondsSpan = clock.querySelector('.js-Countdown-seconds');
 
-//   function updateClock() {
-//     var t = getTimeRemaining(deadline);
+  function updateClock() {
+    var t = getTimeRemaining(deadline);
 
-//     daysSpan.innerHTML = t.days;
-//     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-//     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-//     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-//     if (t.total <= 0) {
-//       clearInterval(timeinterval);
-//     }
-//   }
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
 
-//   updateClock();
-//   var timeinterval = setInterval(updateClock, 1000);
-// }
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
 
-// initializeClock('js-countdown', deadline);
+initializeClock('js-countdown', deadline);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
